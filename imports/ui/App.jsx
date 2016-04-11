6f3efab78da5a7930e28cd013df2da92fd17319f
tintoverano <tintoverano@gmail.com>
2016-04-11 13:41:53 +0200

    request returns sg from feed url

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { feedparser } from 'feedparser';
+import { Meteor } from 'meteor/meteor';
+import feedparser from 'feedparser';
 
 // App component - represents the whole app
 export default class App extends Component {
 
   renderFeed() {
+    //const sceneFeed = "https://www.scenetime.com/get_rss.php?user=tintoverano&cat=51,47,59,80,57,103,1,9,100,43,77,2&passkey=8b7df9f8c0355fb58eb65f9496dbd41e";
+    //const sceneFeed = "http://index.hu/24ora/rss";
+
+    //const aFeedparser = new feedparser();
+
+    Meteor.call('getFeed', (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+        console.log("getFeed method success");
+      }
+    });
     return "a feed";
   }
 
   render() {
     return (
       <div className="container">
-          {this.renderFeed()}
+        {this.renderFeed()}
       </div>
     );
   }
--- server/main.js
+++ server/main.js
@@ -1,5 +1,30 @@
 import { Meteor } from 'meteor/meteor';
+import request from 'request'
+import feedparser from 'feedparser';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+function done(err, result){
+    if(err){
+        console.log(err);
+    } else if(result) {
+        console.log(result);
+    };
+};
+
+function getFeed(done){
+  const sceneFeed = "https://www.scenetime.com/get_rss.php?user=tintoverano&cat=51,47,59,80,57,103,1,9,100,43,77,2&passkey=8b7df9f8c0355fb58eb65f9496dbd41e";
+  const req = request(sceneFeed, []);
+  const aFeedparser = new feedparser();
+
+  return "valami";
+};
+
+
+Meteor.methods({
+  getFeed: function() {
+    return getFeed (done);
+  }
 });
