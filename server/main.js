3d09641a0646539c684bdb1626d2638c55f4fa01
tintoverano <tintoverano@gmail.com>
2016-04-11 13:54:32 +0200

    feed items are coming in stream

--- server/main.js
+++ server/main.js
@@ -19,7 +19,31 @@ function getFeed(done){
   const req = request(sceneFeed, []);
   const aFeedparser = new feedparser();
 
-  return "valami";
+  req.on('error', function (error) {
+    // handle any request errors
+  });
+  req.on('response', function (res) {
+    var stream = this;
+
+    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+
+    stream.pipe(aFeedparser);
+  });
+
+
+  aFeedparser.on('error', function(error) {
+    // always handle errors
+  });
+  aFeedparser.on('readable', function() {
+    // This is where the action is!
+    var stream = this
+      , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+      , item;
+
+    while (item = stream.read()) {
+      console.log(item);
+    }
+  });
 };
 
 
